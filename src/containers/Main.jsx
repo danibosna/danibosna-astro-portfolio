9fb2d43aade99d398411581dcae52cf6e921078e
danibosna <daniel.d.boscan@gmail.com>
2023-11-20 00:03:18 -0600

    [APP] -> first commit

---
+++ src/containers/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '@styles/sass/containers/Main.module.sass';
+
+const Main = () => {
+
+  return (
+    <section className={styles.main}>
+      <div id="resultado" className={styles.resultado}>
+
+      </div>
+    </section>
+  )
+}
+
+export default Main;
